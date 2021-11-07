d65d7358564d609ea5c4183ed68d254cbdd21103
Ruxhino <ruxhino.balliu@fshnstudent.info>
2021-11-07 20:25:36 +0100

    update

--- access_cart_report.py
+++ access_cart_report.py
@@ -1,9 +1,15 @@
 from datetime import datetime
 
-format_date = "%d/%m/%Y"
+format_date = "%m/%d/%Y"
+format_time = '%H:%M:%S'
+t = '10:04:00'
+
+time1 = datetime.strptime(t,format_time)
 
 filename = 'orari.txt'
 i = 1
+
+
 
 
 def is_date(first_value_in_line):
@@ -15,6 +21,8 @@ def is_date(first_value_in_line):
 
 
 def calculate_access_cart_report():
+    time_totali = datetime.strptime('00:00:01', format_time) - datetime.strptime('00:00:01', format_time)
+
     with open(filename) as orari:
         lines = orari.readlines()
         # print(lines)
@@ -25,16 +33,27 @@ def calculate_access_cart_report():
             # print(first_value_str)
 
             if is_date(first_value_str):
-                pass
                 first_value_str_list = first_value_str.split('/')
                 # print(first_value_str_list[2], first_value_str_list[1], first_value_str_list[0])
-                d = datetime(int(first_value_str_list[2]), int(first_value_str_list[1]), int(first_value_str_list[0]))
-
-                if 0 < d.weekday() < 5:
+                d = datetime(int(first_value_str_list[2]), int(first_value_str_list[0]), int(first_value_str_list[1]))
+                # print(first_value_str_list)
+                if d.weekday() < 5:
                     # new_rrjesht = str(lines[line + 1])
-                    print(
-                        f'{lines[line].split()[0:1][0]} => eshte dite jave , {lines[line + 2].split()[0:1][0]} => eshte orari')
-                    pass
+                    #print(f'{lines[line].split()[0:1][0]} => eshte dite jave , {lines[line + 2].split()[0:1][0]} => eshte orari')
+                    t2 = str(lines[line + 2].split()[0:1][0])
+                    # print(t2)
+                    time2 = datetime.strptime(t2,format_time)
+                    if time2 > time1:
+
+                        delta_time =  time2 - time1
+                        # print(delta_time)
+                        time_totali = time_totali + delta_time
+                        print(time_totali)
+                    else:
+                        delta_time = time1 - time2
+                        # print(delta_time)
+                        time_totali = time_totali - delta_time
+                        print(time_totali)
 
 
 # print(is_date('05:45:17'))
